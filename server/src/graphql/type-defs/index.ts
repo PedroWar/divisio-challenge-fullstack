aa4b563b67cb03b21b2e043585e182212d87a9ff
Pedro Pinto <pedro.pinto@ketra.com.br>
2021-10-15 19:04:46 -0300

    add type def

--- server/src/graphql/type-defs/index.ts
+++ server/src/graphql/type-defs/index.ts
@@ -1,7 +1,9 @@
 import { mergeTypeDefs } from '@graphql-tools/merge'
 
 import accountTypeDefs from './account'
+import pokeTypeDefs from './pokemon'
 
 export default mergeTypeDefs([
-  accountTypeDefs
+  accountTypeDefs,
+  pokeTypeDefs
 ])
