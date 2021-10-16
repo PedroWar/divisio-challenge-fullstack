d6ceb1c1f11bf3b05a6d11c593bc12ceae2b7c90
Pedro Pinto <pedro.pinto@ketra.com.br>
2021-10-16 00:37:26 -0300

    add detail to Pokemon type def

--- server/src/graphql/type-defs/pokemon.ts
+++ server/src/graphql/type-defs/pokemon.ts
@@ -2,12 +2,19 @@ import { gql } from 'apollo-server-express'
 
 export default gql`
   type Pokemon {
-    name: String,
-    id: String
-  }
+    id: String!
+    name: String!
+    forms: [Pokemon]
+    spriteFront: String
+    spriteFrontShiny: String
+    typeList:[String!]
+    weight: Int
+    height: Int
+  } 
 
   type Query {
-   pokemons: [Pokemon]
+    pokemons: [Pokemon]
+    pokemon(id: String!): Pokemon
   }
 
 `
