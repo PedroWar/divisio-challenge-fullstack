c243dfe364b5fb8b45a1c808d1efd56add7d8e66
Pedro Pinto <pedro.pinto@ketra.com.br>
2021-10-16 19:27:39 -0300

    add ui features

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,30 @@
 import { GlobalStyle } from './style-guide/GlobalStyle'
 import * as S from './styles'
 import { PokemonList } from './Provider/PokeProvider'
-const App = () => (
-  <>
-    <GlobalStyle />
-    <S.Container>
-      <S.Header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <S.Link>Learn React</S.Link>
-      </S.Header>
-      <PokemonList />
-    </S.Container>
-  </>
-)
+import React, { useRef } from 'react';
+
+const App = () => {
+  const scollToRef: any = useRef();
+  
+  return (
+    <>
+      <GlobalStyle />
+      <S.Container>
+        <S.Header>
+          <S.Poke />
+          <p> 
+            Aqui está a sua pokedex!
+          </p>
+          <S.Link onClick={() => scollToRef.current.scrollIntoView({behavior: "smooth"})}>↓</S.Link>
+        </S.Header>
+        <div ref={scollToRef}>
+          <S.Container>
+            <PokemonList />
+          </S.Container>
+        </div>
+      </S.Container>
+    </>
+  )
+}
 
 export default App
