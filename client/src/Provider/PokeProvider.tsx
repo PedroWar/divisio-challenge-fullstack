12cd390d9fc05c75dc38606b2b63a8eb4e870944
Pedro Pinto <pedro.pinto@ketra.com.br>
2021-10-16 19:27:09 -0300

    add poke provider

--- client/src/Provider/PokeProvider.tsx
+++ client/src/Provider/PokeProvider.tsx
@@ -18,9 +18,30 @@ export function PokemonList() {
     pokeList = data.pokemons;
 
     return pokeList.map((pokemon : any) =>
-        <S.row key={pokemon.id}>
-            <h1>{pokemon.id}</h1> - 
-            <h3>{pokemon.name}</h3>
+        <S.row key={pokemon.id} >
+          <S.cell>
+            <S.id>{pokemon.id}</S.id> 
+            <S.name>{pokemon.name}</S.name>
+          </S.cell>
+        </S.row>
+    );
+  }
+
+  export function PokemonDetail() {
+    const { loading, error, data } = useQuery(GET_POKEMONS);
+
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+    
+    let pokeList = [];
+    pokeList = data.pokemons;
+
+    return pokeList.map((pokemon : any) =>
+        <S.row key={pokemon.id} >
+          <S.cell>
+            <S.id>{pokemon.id}</S.id> 
+            <S.name>{pokemon.name}</S.name>
+          </S.cell>
         </S.row>
     );
   }
