e79ff67d705d1b8139481625ce1f80d5f168191e
Pedro Pinto <pedro.pinto@ketra.com.br>
2021-10-16 00:38:01 -0300

    add req and data treatment

--- server/src/api/pokemonApi.ts
+++ server/src/api/pokemonApi.ts
@@ -1,5 +1,4 @@
 import { RESTDataSource, RequestOptions } from 'apollo-datasource-rest';
-
 export class PokeApi extends RESTDataSource {
   baseURL = 'https://pokeapi.co/api/v2';
 
@@ -9,16 +8,39 @@ export class PokeApi extends RESTDataSource {
 
   async getPokedex(limit = 10) {
     let results = []
-    
     const data = await this.get('/pokemon?limit=20/', {
       per_page: limit,
       order_by: 'name',
-    });
+    })
 
     results = data.results    
-    results.map(result=>result.id = result.url.split("/")[6])    
-
-    return data.results;
-
+    return results.map(x => x = this.prepareReq(x))
   }
+
+  async getPokemon(id: Number) {
+    const data = await this.get(`/pokemon/${id}`, {    });
+    
+    return this.prepareReq(data)
+  }
+
+  prepareReq(pokemon: any) : any{
+    if(pokemon.id === undefined)
+      pokemon.id = pokemon.url.split("/")[6]
+    
+    if(pokemon.forms !== undefined)
+      pokemon.forms.map(form => form = form.name)
+
+    if(pokemon.sprites !== undefined){
+      pokemon.spriteFront = pokemon.sprites.front_default
+      pokemon.spriteFrontShiny = pokemon.sprites.front_shiny
+    }
+
+    if(pokemon.types !== undefined) {
+      pokemon.typeList = pokemon.types.map(type=> type.type.name)
+
+      console.log("TIPO", pokemon.types)
+    }
+    return pokemon
+  }
+
 }
